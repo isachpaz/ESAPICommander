f4cc79e11136217a0d8f307c624c15beb957165c
Ilias Sachpazidis <ilias.sachpazidis@uniklinik-freiburg.de>
2020-10-30 16:00:53 +0100

    ESAPICommander.Tests project added

---
+++ ESAPICommander.Tests/LoggerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ESAPICommander.Logger;
+using NUnit.Framework;
+
+namespace ESAPICommander.Tests
+{
+    [TestFixture]
+    public class LoggerTests
+    {
+        private Action<string> DummyConsole { get; set; }
+        private List<string> Repository { get; set; } 
+
+        [SetUp]
+        public void Init()
+        {
+            Repository = new List<string>();
+            DummyConsole = s => Repository.Add(s);
+        }
+
+        [Test]
+        public void Test_LogConsole_AddInfo()
+        {
+            ILog logger = new LogConsole(DummyConsole);
+            logger.AddInfo("Application started");
+
+            Assert.AreEqual(Repository.Any(), true);
+            Assert.AreEqual(Repository[0], "Application started");
+        }
+    }
+}
