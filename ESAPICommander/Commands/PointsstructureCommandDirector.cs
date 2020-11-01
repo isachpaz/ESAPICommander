c7e1bbfb68768b1f633e46cee2e3a5abbf85cd97
Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2020-11-02 00:51:45 +0100

    PointsstructureArgOptions added

--- ESAPICommander/Commands/PointsstructureCommandDirector.cs
+++ ESAPICommander/Commands/PointsstructureCommandDirector.cs
@@ -1,17 +1,23 @@
-﻿using ESAPICommander.Logger;
+﻿using ESAPICommander.ArgumentConfig;
+using ESAPICommander.Logger;
 using ESAPICommander.Proxies;
 
 namespace ESAPICommander.Commands
 {
     public class PointsstructureCommandDirector : BaseCommandDirector
     {
+        private PointsstructureArgOptions _options;
+
+        public PointsstructureCommandDirector(PointsstructureArgOptions options, IEsapiCalls esapi, ILog log) :
+            base(esapi, log)
+        {
+            _options = options;
+        }
+
         public override int Run()
         {
             throw new System.NotImplementedException();
         }
 
-        public PointsstructureCommandDirector(IEsapiCalls esapi, ILog log) : base(esapi, log)
-        {
-        }
     }
 }
